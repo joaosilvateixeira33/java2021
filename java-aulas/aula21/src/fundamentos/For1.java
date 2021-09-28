40a256f5812cf26a50f0eb1ae868232798fc65de
joaosilvateixeira33 <87495429+joaosilvateixeira33@users.noreply.github.com>
2021-09-28 16:46:00 -0300

    update

---
+++ java-aulas/aula21/src/fundamentos/For1.java
@@ -0,0 +1,21 @@
+package fundamentos;
+
+public class For1 {
+	public static void main(String[] args) {
+//		int contador = 0;
+//		while(contador <= 20) {
+//			System.out.printf("contando: %d\n", contador);
+//			contador += 2;
+//		}
+		
+		for(int contador = 0; contador <= 20; contador += 2) {
+			System.out.printf("contando: %d\n", contador);
+		}
+		
+		//laço infinito
+//		for(;true;) {
+//			System.out.println("Fim");
+//		}
+	}
+	
+}
